53341212fc5ef76f0d3200ac3e71befb736562c1
vijaypaliwal <vijay.paliwal@hotmail.com>
2016-10-19 11:48:59 +0530

    update
    new updated

--- js/index.js
+++ js/index.js
@@ -34,20 +34,18 @@ var app = {
     // function, we must explicitly call 'app.receivedEvent(...);'
     onDeviceReady: function() {
         app.receivedEvent('deviceready');
-        stepcounter.getHistory(
-           function (historyData) {
-               success(historyData);
-           },
-               failure
-           );
-
-        var success = function (message) {
-            alert(message);
-        }
-
-        var failure = function () {
-            alert("Error calling CordovaStepCounter Plugin");
-        }
+        var successHandler = function (pedometerData) {
+            alert(pedometerData.numberOfSteps)
+            // pedometerData.numberOfSteps;
+            // pedometerData.distance;
+            // pedometerData.floorsAscended;
+            // pedometerData.floorsDescended;
+        };
+        var options = {
+            "startDate": new Date("Fri May 01 2015 15:20:00"),
+            "endDate": new Date("Fri May 01 2015 15:25:00")
+        };
+        pedometer.queryData(successHandler, onError, options);
 
     },
     // Update DOM on a Received Event
