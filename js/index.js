734811732ada13601fa37d7e023cf2130afc8632
vijaypaliwal <vijay.paliwal@hotmail.com>
2017-04-06 15:44:23 +0530

    update for shivSaagar
    update for shiv saagar

--- js/index.js
+++ js/index.js
@@ -18,28 +18,25 @@
  */
 var app = {
     // Application Constructor
-    initialize: function () {
+    initialize: function() {
         this.bindEvents();
     },
     // Bind Event Listeners
     //
     // Bind any events that are required on startup. Common events are:
     // 'load', 'deviceready', 'offline', and 'online'.
-    bindEvents: function () {
+    bindEvents: function() {
         document.addEventListener('deviceready', this.onDeviceReady, false);
     },
     // deviceready Event Handler
     //
     // The scope of 'this' is the event. In order to call the 'receivedEvent'
     // function, we must explicitly call 'app.receivedEvent(...);'
-    onDeviceReady: function () {
+    onDeviceReady: function() {
         app.receivedEvent('deviceready');
-        alert("device ready function");
-        
-
     },
     // Update DOM on a Received Event
-    receivedEvent: function (id) {
+    receivedEvent: function(id) {
         var parentElement = document.getElementById(id);
         var listeningElement = parentElement.querySelector('.listening');
         var receivedElement = parentElement.querySelector('.received');
@@ -47,21 +44,6 @@ var app = {
         listeningElement.setAttribute('style', 'display:none;');
         receivedElement.setAttribute('style', 'display:block;');
 
-       // console.log('Received Event: ' + id);
-        var startingOffset = 0;
-        stepcounter.start(startingOffset, function (message) {
-            alert("started");
-        }, failure);
-        stepcounter.getTodayStepCount(function (message) {
-            alert(message);
-        }, failure);
-
-        
-        var failure = function () {
-            alert("Error calling CordovaStepCounter Plugin");
-        }
-
-        //stepcounter.stop(success, failure);
-
+        console.log('Received Event: ' + id);
     }
 };
